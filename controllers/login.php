f39769c09140129035ed3715fe7b8d1e49023c9c
Alexiotovv <gpsalexvasquez@gmail.com>
2025-03-30 14:22:43 -0500

    login_rollback

--- controllers/login.php
+++ controllers/login.php
@@ -36,22 +36,6 @@ class Login extends Controller {
 	
 	function run()
 	{	
-		// Si el resultado es un número (por ejemplo, 4 = usuario no encontrado)
-    if (is_numeric($result)) {
-        echo json_encode([
-            'status' => 'error',
-            'message' => 'Usuario o contraseña incorrectos'
-        ]);
-        return;
-    }
-
-    // Si el resultado es un array con los datos de sesión
-    echo json_encode([
-        'status' => 'ok',
-        'message' => 'Login exitoso',
-        'session' => $_SESSION // Para verificar si las sesiones se están estableciendo bien
-    ]);
-
 		print_r(json_encode($this->model->run($_POST)));
 	}
 	
